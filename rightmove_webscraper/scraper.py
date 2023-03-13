72cd880f7cc0090455ddc63178e9ce8088b46880
chloerulesok <chloe.sanderson@btopenworld.com>
2023-03-13 18:45:50 +0000

    Testing

--- rightmove_webscraper/scraper.py
+++ rightmove_webscraper/scraper.py
@@ -215,10 +215,10 @@ class RightmoveData:
                         script_list.append(res_jsoncapture[0])
                         #New stuff
 
-                        property_data = json.loads(str(res_jsoncapture[0]))
+                        property_data = json.loads(res_jsoncapture[0])
                         #prices = property_data['propertyData']
                         #price = property_data.get("propertyData")
-                        primary_prices.append(property_data.keys())
+                        primary_prices.append(type(property_data))
 
 
 
